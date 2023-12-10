152365e89916b7df4c4da2223c659659f0472af9
mgabr <marcosgabriel79355@gmail.com>
2023-12-10 13:40:31 -0300

    Começando a criar método de efetuar login

--- src/main/java/com/estudos/Estudosspring/service/UsuarioService.java
+++ src/main/java/com/estudos/Estudosspring/service/UsuarioService.java
@@ -20,5 +20,20 @@ public class UsuarioService {
         usuarioRepository.save(usuario);
     }
 
-    // Adicione métodos adicionais conforme necessário para operações relacionadas a usuários
+    public String fazerLogin(LoginRequest loginRequest) {
+        // Busca o usuário pelo email
+        Usuario usuario = usuarioRepository.findByEmail(loginRequest.getEmail());
+
+        // Verifica se o usuário existe
+        if (usuario != null) {
+            // Verifica se a senha fornecida corresponde à senha armazenada (considerando que você usa bcrypt)
+            if (passwordEncoder.matches(loginRequest.getSenha(), usuario.getSenha())) {
+                return "Login bem-sucedido!";
+            } else {
+                return "Senha incorreta.";
+            }
+        } else {
+            return "Usuário não encontrado.";
+        }
+    }
 }
