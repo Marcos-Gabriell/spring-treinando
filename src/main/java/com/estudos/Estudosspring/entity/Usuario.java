e9aac874206d950b1682c1fb1d1d344e52bc9d24
mgabr <marcosgabriel79355@gmail.com>
2023-12-10 13:36:56 -0300

    Projeto rodando e cadastrando usuario.

--- src/main/java/com/estudos/Estudosspring/entity/Usuario.java
+++ src/main/java/com/estudos/Estudosspring/entity/Usuario.java
@@ -1,18 +1,29 @@
 package com.estudos.Estudosspring.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.Data;
 
 @Entity
+@Table(name = "Usuario")
 @Data
 public class Usuario {
 
     @Id
+    @Column(length = 255)
     private String email;
+
+    @Column(length = 255)
     private String nome;
+
+    @Column(length = 255)
     private String sobrenome;
+
     private int idade;
+
+    @Column(length = 255)
     private String senha;
 }
