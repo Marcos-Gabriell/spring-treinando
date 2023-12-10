9fdb830829b57cf8d6c2b6db404de21610e4ff9b
mgabr <marcosgabriel79355@gmail.com>
2023-12-10 14:24:52 -0300

    implementando loombok

--- src/main/java/com/estudos/Estudosspring/dto/LoginRequest.java
+++ src/main/java/com/estudos/Estudosspring/dto/LoginRequest.java
@@ -1,37 +1,16 @@
 package com.estudos.Estudosspring.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class LoginRequest {
 
     private String email;
     private String senha;
-
-    // Construtor vazio
-    public LoginRequest() {
-    }
-
-    // Construtor com parâmetros
-    public LoginRequest(String email, String senha) {
-        this.email = email;
-        this.senha = senha;
-    }
-
-    // Getter para o email
-    public String getEmail() {
-        return email;
-    }
-
-    // Setter para o email
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    // Getter para a senha
-    public String getSenha() {
-        return senha;
-    }
-
-    // Setter para a senha
-    public void setSenha(String senha) {
-        this.senha = senha;
-    }
 }
